ba2f3c9351aeaea5d38103f32a098a67be0f5de9
Mu-Zi-Li <71595032+Li-He-Yu@users.noreply.github.com>
2023-06-04 17:28:21 +0800

    Update manager_mode.h

--- manager_mode.h
+++ manager_mode.h
@@ -1,9 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "database.h"
+#ifndef MANAGER_MODE_H
+#define MANAGER_MODE_H
 
-void interactive_place(void);
-void search_question_interactive(void);
+void manager_mode(void);
 int check_answer(int ques_id, int player_answer);
 void modify_question_correctness_percent(int operation, int id);
+
+#endif
